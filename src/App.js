1a0fa5a87ac6b27be7ccab96d4c32c4ea5ff321b
heyjochen <jochen@stierberger.com>
2022-06-21 19:56:52 -0500

    modify state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import React from "react";
+import Box from "./Box";
 import boxes from "./boxes";
+
 import "./App.css";
 
 function App() {
-  return boxes.map((box) => <div key={box.id} className="box"></div>);
+  const [squares, setSquares] = useState(boxes);
+
+  const squareElements = squares.map((box) => <Box key={box.id} on={box.on} />);
+
+  return <>{squareElements}</>;
 }
 
 export default App;
