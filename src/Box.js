ca38963889769c013aedc260ed29aadb0bc9ea29
heyjochen <jochen@stierberger.com>
2022-06-21 19:57:04 -0500

    define style for box

--- src/Box.js
+++ src/Box.js
@@ -1,7 +1,17 @@
-import boxes from "./boxes";
+import React from "react";
 
-const Box = () => {
-  boxes.map((box) => <div key={box.id} className="box"></div>);
+const Box = ({ on }) => {
+  const [isOn, setIsOn] = React.useState(on);
+
+  const onOrOff = () => {
+    setIsOn((prev) => !prev);
+  };
+
+  const styles = {
+    backgroundColor: isOn ? "#222222" : "#e2e2e2",
+  };
+
+  return <div className="box" onClick={onOrOff} style={styles}></div>;
 };
 
 export default Box;
